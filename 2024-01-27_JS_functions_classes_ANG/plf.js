12cd171d6b5307f0dff97462049ce556d3329c45
Paula Mariana <DUM230093@spengergasse.at>
2024-02-15 12:25:01 +0100

    commit

--- 2024-01-27_JS_functions_classes_ANG/plf.js
+++ 2024-01-27_JS_functions_classes_ANG/plf.js
@@ -1,4 +1,18 @@
-class Person (){
-    constructor (name, )
+class Person {
+    constructor (name, geschlecht, groesseM, gewichtKG) {
+        this.name = name;
+        this.geschlecht = geschlecht;
+        this.groesseM = groesseM;
+        this.gewichtKG = gewichtKG;
+        if (geschlecht !== "m" && geschlecht ! == "f") {
+            throw new Error("Wrong gender");
+        }
+    }
+toString(){
+    return "John Doe (180cm, 80kg)";
 }
+
+} 
+    
+
 export { Person };
